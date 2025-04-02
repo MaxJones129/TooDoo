08400ea007ed918d9639b5500317d9b0112d504a
MaxJones129 <maxjones129@gmail.com>
2025-04-01 19:05:29 -0500

    Initial Commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,13 +1,20 @@
 'use client';
 
- // any component that uses useAuth needs this because if a component directly imports useAuth, it needs to be a client component since useAuth uses React hooks.
-
+// any component that uses useAuth needs this because if a component directly imports useAuth, it needs to be a client component since useAuth uses React hooks.
+import React, { useEffect, useState } from 'react';
 import { Button } from 'react-bootstrap';
-import { signOut } from '@/utils/auth'; // anything in the src dir, you can use the @ instead of relative paths
-import { useAuth } from '@/utils/context/authContext';
+// import { signOut } from '@/utils/auth'; // anything in the src dir, you can use the @ instead of relative paths
+// import { useAuth } from '@/utils/context/authContext';
+import ToDoCard from '../components/ToDoCard';
+import getTodos from '../api/todosData';
 
 function Home() {
-  const { user } = useAuth();
+  // const { user } = useAuth();
+  const [todos, setTodos] = useState([]);
+
+  useEffect(() => {
+    getTodos().then(setTodos);
+  }, []);
 
   return (
     <div
@@ -19,11 +26,11 @@ function Home() {
         margin: '0 auto',
       }}
     >
-      <h1>Hello {user.displayName}! </h1>
-      <p>Click the button below to logout!</p>
-      <Button variant="danger" type="button" size="lg" className="copy-btn" onClick={signOut}>
-        Sign Out
-      </Button>
+      {todos.map((todo) => (
+        <ToDoCard key={todo.firebaseKey} toDoObj={todo}>
+          <Button>To Did Done</Button>
+        </ToDoCard>
+      ))}
     </div>
   );
 }
